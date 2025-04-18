f3e1abaa18f64b80954a692d0bb8e5a3bd22eed2
Oleg <zhukov.ol3q@yandex.ru>
2025-04-18 22:01:37 +0300

    dsds

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import openai
 import requests
 
-API_KEY = "sk-4ac17203bf044f3b9fb922b5270ae975"
-API_URL = "https://api.deepseek.com/v1/chat/completions"
+openai.api_base = "https://api.deepseek.com/v1"
 
-headers = {
-    "Authorization": f"Bearer {API_KEY}",
-    "Content-Type": "application/json"
-}
-
+openai.api_key = "sk-4ac17203bf044f3b9fb922b5270ae975"
 
 def get_label_from_deepseek(message):
     system_prompt = (
-        "Ты классификатор. Отвечай ТОЛЬКО в формате {'label': LABEL_{I}}, "
-        "где I — одна из категорий: phishing(I = 0), spam(I = 1),  manipulation (I = 2), safe(I=3)."
+        "Ты классификатор. Отвечай ТОЛЬКО в формате {'label': {I}}, "
+        "где I — одна из категорий: phishing(I = 0), spam(I = 1), manipulation (I = 2), safe(I=3)."
     )
 
     data = {
-        "model": "deepseek-chat",  # или "deepseek-coder" если используешь кодовую модель
+        "model": "deepseek-chat",
         "messages": [
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": message}
@@ -29,13 +25,13 @@ def get_label_from_deepseek(message):
         "temperature": 0
     }
 
-    response = requests.post(API_URL, headers=headers, json=data)
+    try:
+        response = openai.ChatCompletion.create(**data)
 
-    if response.status_code == 200:
-        result = response.json()
-        return result['choices'][0]['message']['content']
-    else:
-        print("Ошибка:", response.status_code, response.text)
+        result = response['choices'][0]['message']['content']
+        return result
+    except openai.Error as e:
+        print("Ошибка:", e)
         return None
 
 app = FastAPI()
@@ -56,5 +52,5 @@ def validate(data: Message):
     text = data.message
     label_response = get_label_from_deepseek(text)
     label_json = json.loads(label_response.replace("'", '"'))
-    print(label_json)   
-    return {"class": int(label_json["label"][6:])}
+    print(label_json)
+    return {"class": int(label_json["label"])}
